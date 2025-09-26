d99f64228cbb11762bee0dc053bed38d97bdd6f2
skomar70 <mosharofroydev@gmail.com>
2025-09-26 14:09:08 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/AutoFilterAdvance /AutoFilterAdvance
+  git clone https://github.com/VJBots/Auto-filterbot /Auto-filterbot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AutoFilterAdvance
 fi
-cd /AutoFilterAdvance
+cd /Auto-filterbot
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
