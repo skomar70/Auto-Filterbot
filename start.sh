0cb489da5db863d4d43c48deafaf98d87a77ad59
skomar70 <mosharofroydev@gmail.com>
2025-09-26 16:24:44 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,15 @@
-if [ -z $UPSTREAM_REPO ]
-then
+#!/bin/bash
+
+if [ -z "$UPSTREAM_REPO" ]; then
   echo "Cloning main Repository"
-  git clone https://github.com/skomar70/Auto-filterbot/Auto-filterbot
+  git clone https://github.com/skomar70/Auto-filterbot.git /Auto-filterbot
+  cd /Auto-filterbot
 else
-  echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /AutoFilterAdvance
+  echo "Cloning Custom Repo from $UPSTREAM_REPO"
+  git clone "$UPSTREAM_REPO" /AutoFilterAdvance
+  cd /AutoFilterAdvance
 fi
-cd /Auto-filterbot
+
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
