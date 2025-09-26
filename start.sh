77542ab6627a231134f5c3bd4c9ae0f8e020d871
skomar70 <mosharofroydev@gmail.com>
2025-09-26 10:14:01 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /AutoFilterAdvance
 fi
-cd /AutoFilterAdvance
+cd /Auto-filterbot
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
