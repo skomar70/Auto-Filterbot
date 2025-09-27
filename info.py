c3754f88feb27e24e7247fe7e8bb332534dee52e
skomar70 <mosharofroydev@gmail.com>
2025-09-27 03:00:54 +0000

    Update info.py

--- info.py
+++ info.py
@@ -45,7 +45,7 @@ JOIN_REQS_DB = environ.get("JOIN_REQS_DB", DATABASE_URI)
 
 # Others
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002965828075'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'VJ_Bot_Disscussion')
+SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'request_gruop')
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "True")), False)
 IMDB = is_enabled((environ.get('IMDB', "False")), True)
 SINGLE_BUTTON = is_enabled((environ.get('SINGLE_BUTTON', "True")), False)
