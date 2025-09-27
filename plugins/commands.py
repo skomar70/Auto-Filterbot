b1c12d61ec8514f1ad005a741f67e45a705c44b5
skomar70 <mosharofroydev@gmail.com>
2025-09-27 08:47:38 +0000

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -24,7 +24,7 @@ async def start(client, message):
     if message.chat.type in [enums.ChatType.GROUP, enums.ChatType.SUPERGROUP]:
         buttons = [
             [
-                InlineKeyboardButton("Updates", url="https://t.me/VJ_Bots"),
+                InlineKeyboardButton("Winner Group", url="https://t.me/VJ_Bots"),
                 InlineKeyboardButton("🍿 YouTube Channel 🍿", url="https://youtube.com/@Tech_VJ")
             ]
             ]
