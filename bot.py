dcb49ae7c515fe31fce2eed75a7e1ee169f2248b
skomar70 <mosharofroydev@gmail.com>
2025-09-27 18:45:01 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -15,6 +15,7 @@ from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR
 from utils import temp
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
+from ads import send_ads
 
 class Bot(Client):
 
