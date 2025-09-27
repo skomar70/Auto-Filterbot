78b50f0f8ec0a0bc843c6ada8121ccaa81b56cfb
skomar70 <mosharofroydev@gmail.com>
2025-09-27 09:50:14 +0000

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -97,12 +97,12 @@ async def next_page(bot, query):
     elif off_set is None:
         btn.insert(0,
             [
-                InlineKeyboardButton("Updates", url="https://t.me/searchhelptm"),
+                InlineKeyboardButton("Join the support group", url="https://t.me/request_gruop"),
             ]
         )
         btn.insert(1,
             [
-                InlineKeyboardButton("🍿 Join the support group 🍿", url="https://t.me/request_gruop"),
+                InlineKeyboardButton("🍿Update Channel🍿", url="https://t.me/searchhelptm"),
             ]
         )
         btn.append(
